3abb8944d5e47ef3b7576c7d78c3082e538f5dd8
SRH <srholand@gmail.com>
2024-10-10 17:21:35 -0500

    basics

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { OpenAI } from 'openai';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "testsharp" is now active!');
+    // Register the first command: "Test Me"
+    let disposableTestMe = vscode.commands.registerCommand('extension.testMe', () => {
+        console.log('I was Tested');
+        vscode.window.showInformationMessage('I was Tested');
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('testsharp.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TestSharp!');
-	});
+    // Register the second command: "Another Test"
+    let disposableAnotherTest = vscode.commands.registerCommand('extension.anotherTest', () => {
+        const openai = new OpenAI({ apiKey: '' });
 
-	context.subscriptions.push(disposable);
+        console.log('Another Test was Triggered');
+        vscode.window.showInformationMessage('Another Test was Triggered');
+    });
+
+    // Add both commands to the context subscriptions
+    context.subscriptions.push(disposableTestMe);
+    context.subscriptions.push(disposableAnotherTest);
 }
 
 // This method is called when your extension is deactivated
